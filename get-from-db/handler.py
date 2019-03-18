6eabdce54554836c95f1712c9d7041d3cccd9736
trustdark <trustindark@gmail.com>
2019-03-18 16:17:02 +0200

    files related to jupyter moved to jupyter-notebook directory, and deleted from / READMY.md and get-from-db/handler.py updated

--- get-from-db/handler.py
+++ get-from-db/handler.py
@@ -1,12 +1,13 @@
 import pymongo
 import json
 import datetime
-
-arg = json.dumps({'city':'Kyiv', 'data': 'hum', 'val_min': 10, 'val_max': 90,
-       'time_min': '2019-03-16 11:16:12', 'time_max': '2019-03-16 17:30:12'})
+#request structure which comes from jupyter notebook
+#arg = json.dumps({'city':'Kyiv', 'data': 'hum', 'val_min': 10, 'val_max': 90,
+#       'time_min': '2019-03-16 11:16:12', 'time_max': '2019-03-16 17:30:12'})
 
 def handle(req):
 
+#code connects to mongodb
     secret = ''
     with open('/var/openfaas/secrets/secret-api-key') as fin:
         for line in fin:
@@ -19,10 +20,10 @@ def handle(req):
     coll_1 = db["weather"]
     coll_2 = db["seismicity"]
 
+#parsing request from jupyter notebook and querying data from Mongodb
     arg = json.loads(req)
 
     if arg['data'] in ["temp", "hum", "v_wind"]:
-
         array_unprepared = list(coll_1.find({'city': arg['city'], "$and": [{arg["data"]: {"$gte": arg["val_min"]}}, {arg["data"]: {"$lte": arg["val_max"]}}]},
                                             {"_id": 0, arg["data"]: 1, 'time': 1}))
     else:
@@ -31,6 +32,8 @@ def handle(req):
                                             {"_id": 0, arg["data"]: 1, 'time': 1}))
 
     new_array = []
+    #here you can see piece of code, which compare dates from mongo with date arg's from notebook.
+    #It's happen due to storing dates as strings in mongodb
     for arr_elem in array_unprepared:
         if datetime.datetime.strptime(arg["time_min"], '%Y-%m-%d %X') < datetime.datetime.strptime(arr_elem['time'][0:19], '%Y-%m-%d %X') < datetime.datetime.strptime(arg["time_max"], '%Y-%m-%d %X'):
             new_array.append(arr_elem)
